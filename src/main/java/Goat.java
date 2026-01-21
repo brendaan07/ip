fa33b98a8e265252531183d8d094145c471e5593
brendaan07 <brendaaaan07@gmail.com>
2026-01-21 14:33:19 +0800

    Level 0

--- src/main/java/Goat.java
+++ src/main/java/Goat.java
@@ -1,10 +1,14 @@
-public class Duke {
+public class Goat {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+         */
+        System.out.println("Hello! I'm Goat \n " +
+                "What Can I do for you?  \n "+
+                "Bye. Hope to see you again soon!");
     }
 }
