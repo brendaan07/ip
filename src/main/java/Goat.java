c6e8a561c5f65d1a0c0e9e52ad7436e54895b9c9
brendaan07 <brendaaaan07@gmail.com>
2026-01-22 11:27:54 +0800

    Level 1

--- src/main/java/Goat.java
+++ src/main/java/Goat.java
@@ -1,14 +1,18 @@
+import java.util.Scanner;
+
 public class Goat {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-
-         */
-        System.out.println("Hello! I'm Goat \n " +
-                "What Can I do for you?  \n "+
-                "Bye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Goat Chatbot \n" +
+                "What Can I do for you?  \n"
+        );
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userinput = sc.nextLine();
+            if (userinput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(userinput);
+        }
     }
 }
