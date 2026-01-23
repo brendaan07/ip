2fe10e110dd00c8f2755dff7e3e3df7ed2893d86
brendaan07 <brendaaaan07@gmail.com>
2026-01-23 14:56:40 +0800

    Level-3

--- src/main/java/Goat.java
+++ src/main/java/Goat.java
@@ -6,19 +6,35 @@ public class Goat {
         System.out.println("Hello! I'm Goat Chatbot \n" +
                 "What Can I do for you?  \n"
         );
-        ArrayList<String> lst = new ArrayList<>();
+        ArrayList<Task> lst = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         while (true) {
-            String userinput = sc.nextLine();
-            if (userinput.equals("bye")) {
+            String userinput = sc.nextLine().trim(); //remove trailing spaces
+            String[] parts = userinput.split("\\s+", 2); //split into command and arguments
+            String command = parts[0].toLowerCase();
+            String arguments = parts.length > 1 ? parts[1] : "";
+
+
+            if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (userinput.equals("list")) { //display list
+            } else if (command.equals("list")) { //display list
                 for (int i = 0; i < lst.size(); i++) {
-                    System.out.println((i + 1) + ". " + lst.get(i));
+                    System.out.println((i + 1) + ". " + lst.get(i).toString());
                 }
+            } else if (command.equals("mark")) { //mark task
+                int index = Integer.parseInt(arguments) - 1;
+                Task currTask = lst.get(index);
+                currTask.mark();
+                System.out.println("Nice! I've marked this task as done \n" + currTask);
+            } else if (command.equals("unmark")) {
+                int index = Integer.parseInt(arguments) - 1;
+                Task currTask = lst.get(index);
+                currTask.unmark();
+                System.out.println("Ok, I've marked this task as not done yet \n" + currTask);
             } else {
-                lst.add(userinput);
+                Task newTask = new Task(userinput);
+                lst.add(newTask);
                 System.out.println("added: " + userinput);
             }
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,26 +1,30 @@
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Goat {
-    public static void main(String[] args) {
-        System.out.println("Hello! I'm Goat Chatbot \n" +
-                "What Can I do for you?  \n"
-        );
-        ArrayList<String> lst = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            String userinput = sc.nextLine();
-            if (userinput.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else if (userinput.equals("list")) { //display list
-                for (int i = 0; i < lst.size(); i++) {
-                    System.out.println((i + 1) + ". " + lst.get(i));
-                }
-            } else {
-                lst.add(userinput);
-                System.out.println("added: " + userinput);
-            }
+public class Task {
+    private boolean done;
+    private String name;
+
+    public Task(String name) {
+        this.done = false;
+        this.name = name;
+    }
+
+    public void mark() {
+        this.done = true;
+    }
+
+    public void unmark() {
+        this.done = false;
+    }
+
+    public String toString() {
+        String check = "";
+        if (!this.done) {
+            check = "[ ]";
+        } else {
+            check = "[X]";
         }
+        return check + " " + this.name;
     }
 }
