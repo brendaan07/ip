fce08d88da8e7b04406f0f4f4260241478281322
brendaan07 <brendaaaan07@gmail.com>
2026-01-29 13:10:29 +0800

    More OOP - Parser and Storage

---
+++ src/main/java/Parser.java
@@ -0,0 +1,23 @@
+public class Parser {
+    private final String command;
+    private final String arguments;
+
+    public Parser(String userInput) {
+        String[] parts = userInput.split("\\s+", 2); //split into command and arguments
+        String command = parts[0].toLowerCase();
+        String arguments = parts.length > 1 ? parts[1] : "";
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public String getArguments() {
+        return arguments;
+    }
+
+    //require arguments helper function
+    public static void requireArgs(String args, String command) throws MissingArgumentException {
+        if (args.isEmpty()) throw new MissingArgumentException(command);
+    }
+}
